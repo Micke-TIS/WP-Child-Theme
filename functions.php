ab71f29a17693ed792180e06a9aa5ddde6341ed5
Micke <mikael.johanssonj@gmail.om>
2021-03-08 12:37:09 +0100

    test

--- functions.php
+++ functions.php
@@ -27,37 +27,67 @@ function sf_child_theme_dequeue_style() {
  * Displays the custom text field input field in the WooCommerce product data meta box
  */
  function cfwc_create_custom_field() {
- $args = array(
- 'id' => 'custom_text_field_title',
- 'label' => __( 'Custom Text Field Title', 'cfwc' ),
- 'class' => 'cfwc-custom-field',
- 'desc_tip' => true,
- 'description' => __( 'Enter the title of your custom text field.', 'ctwc' ),
+   $args = array(
+    'id' => 'custom_text_field_title',
+    'label' => __( 'Custom Text Field Title', 'cfwc' ),
+    'class' => 'cfwc-custom-field',
+    'desc_tip' => true,
+    'description' => __( 'Enter the title of your custom text field.', 'ctwc' ),
  );
+  woocommerce_wp_text_input( $args );
+
+  $args = array(
+   'id' => 'custom_text_field_description',
+   'label' => __( 'Custom Text Field Description', 'cfwc' ),
+   'class' => 'cfwc-custom-field',
+   'desc_tip' => true,
+   'description' => __( 'Enter the title of your custom text field.', 'ctwc' ),
+);
  woocommerce_wp_text_input( $args );
- }
+
+
+
+  }
+
  add_action( 'woocommerce_product_options_general_product_data', 'cfwc_create_custom_field' );
  /**
  * Saves the custom field data to product meta data
  */
- function cfwc_save_custom_field( $post_id ) {
- $product = wc_get_product( $post_id );
- $title = isset( $_POST['custom_text_field_title'] ) ? $_POST['custom_text_field_title'] : '';
- $product->update_meta_data( 'custom_text_field_title', sanitize_text_field( $title ) );
- $product->save();
- }
+  function cfwc_save_custom_field( $post_id ) {
+    $product = wc_get_product( $post_id );
+    $title = isset( $_POST['custom_text_field_title'] ) ? $_POST['custom_text_field_title'] : '';
+    $product->update_meta_data( 'custom_text_field_title', sanitize_text_field( $title ) );
+    $product->save();
+
+    $product = wc_get_product( $post_id );
+    $description = isset( $_POST['custom_text_field_description'] ) ? $_POST['custom_text_field_description'] : '';
+    $product->update_meta_data( 'custom_text_field_description', sanitize_text_field( $description ) );
+    $product->save();
+
+
+  }
  add_action( 'woocommerce_process_product_meta', 'cfwc_save_custom_field' );
 
  /**
 * Displays custom field data after the add to cart button
 */
 function cfwc_display_custom_field() {
-global $post;
+  global $post;
+
 // Check for the custom field value
-$product = wc_get_product( $post->ID );
-$title = $product->get_meta( 'custom_text_field_title' );
-if( $title ) {
-echo get_post_meta($post->ID, 'custom_text_field_title', true);
-}
+  $product = wc_get_product( $post->ID );
+  $title = $product->get_meta( 'custom_text_field_title' );
+    if( $title ) {
+      echo get_post_meta($post->ID, 'custom_text_field_title', true);
+    }
+
+    // Check for the custom field value
+      $product = wc_get_product( $post->ID );
+      $description = $product->get_meta( 'custom_text_field_description' );
+        if( $description ) {
+          echo get_post_meta($post->ID, 'custom_text_field_description', true);
+        }
+
+
 }
 add_action( 'woocommerce_after_add_to_cart_button', 'cfwc_display_custom_field' );
