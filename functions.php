c79266a6aa369ff13bf1fe284a3b253631a84b6f
Micke <mikael.johanssonj@gmail.om>
2021-03-08 12:38:47 +0100

    d

--- functions.php
+++ functions.php
@@ -85,7 +85,7 @@ function cfwc_display_custom_field() {
       $product = wc_get_product( $post->ID );
       $description = $product->get_meta( 'custom_text_field_description' );
         if( $description ) {
-          echo get_post_meta($post->ID, 'custom_text_field_description', true);
+          echo get_post_meta($post->ID, '</br> custom_text_field_description', true);
         }
 
 
