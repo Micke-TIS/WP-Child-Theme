34f8e838ac46d3748a51065b8bf8b67010cc21ba
Micke <mikael.johanssonj@gmail.om>
2021-03-08 12:05:01 +0100

    Testing to make the custom fields on front-end

--- functions.php
+++ functions.php
@@ -23,7 +23,13 @@ function sf_child_theme_dequeue_style() {
  * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
  */
 
+// Declaring custom template functioning on woocommerce
+ function mytheme_add_woocommerce_support() {
+ add_theme_support( 'woocommerce' );
+ }
+ add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
+//Making of custom fields on woocommerce product in general tab
  // Display Fields
  add_action('woocommerce_product_options_general_product_data', 'woocommerce_product_custom_fields');
  // Save Fields
@@ -63,4 +69,20 @@ function sf_child_theme_dequeue_style() {
          )
      );
      echo '</div>';
+ }
+// Saving the custom fields in database
+ function woocommerce_product_custom_fields_save($post_id)
+ {
+     // Custom Product Text Field
+     $woocommerce_custom_product_text_field = $_POST['_custom_product_text_field'];
+     if (!empty($woocommerce_custom_product_text_field))
+         update_post_meta($post_id, '_custom_product_text_field', esc_attr($woocommerce_custom_product_text_field));
+ // Custom Product Number Field
+     $woocommerce_custom_product_number_field = $_POST['_custom_product_number_field'];
+     if (!empty($woocommerce_custom_product_number_field))
+         update_post_meta($post_id, '_custom_product_number_field', esc_attr($woocommerce_custom_product_number_field));
+ // Custom Product Textarea Field
+     $woocommerce_custom_procut_textarea = $_POST['_custom_product_textarea'];
+     if (!empty($woocommerce_custom_procut_textarea))
+         update_post_meta($post_id, '_custom_product_textarea', esc_html($woocommerce_custom_procut_textarea));
  }
