54a7b8dd075857ce7325f609cdbcd858f9763812
Micke <mikael.johanssonj@gmail.om>
2021-03-08 13:04:17 +0100

    d

--- functions.php
+++ functions.php
@@ -23,23 +23,51 @@ function sf_child_theme_dequeue_style() {
  * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
  */
 
- /**
-  * Add a custom product data tab
-  */
- add_filter( 'woocommerce_product_tabs', 'woo_new_product_tab' );
- function woo_new_product_tab( $tabs ) {
+ // Add a custom product setting tab to edit product pages options FOR SIMPLE PRODUCTS only
+ add_filter( 'woocommerce_product_data_tabs', 'discount_new_product_data_tab', 50, 1 );
+ function discount_new_product_data_tab( $tabs ) {
+     $tabs['discount'] = array(
+         'label' => __( 'Discount', 'woocommerce' ),
+         'target' => 'discount_product_data', // <== to be used in the <div> class of the content
+         'class' => array('show_if_simple'), // or 'hide_if_simple' or 'show_if_variable'…
+     );
 
-   // Add a custom product setting tab to edit product pages options FOR SIMPLE PRODUCTS only
-   add_filter( 'woocommerce_product_data_tabs', 'discount_new_product_data_tab', 50, 1 );
-   function discount_new_product_data_tab( $tabs ) {
-       $tabs['discount'] = array(
-           'label' => __( 'Discount', 'woocommerce' ),
-           'target' => 'discount_product_data', // <== to be used in the <div> class of the content
-           'class' => array('show_if_simple'), // or 'hide_if_simple' or 'show_if_variable'…
-       );
+     return $tabs;
+ }
 
-       return $tabs;
-   }
+ // Add/display custom Fields in the custom product settings tab
+ add_action( 'woocommerce_product_data_panels', 'add_custom_fields_product_options_discount', 10 );
+ function add_custom_fields_product_options_discount() {
+     global $post;
+
+     echo '<div id="discount_product_data" class="panel woocommerce_options_panel">'; // <== Here we use the target attribute
+
+     woocommerce_wp_text_input(  array(
+         'type'          => 'number', // Add an input number Field
+         'id'            => '_discount_info',
+         'label'         => __( 'Percentage Discount', 'woocommerce' ),
+         'placeholder'   => __( 'Enter the % discount.', 'woocommerce' ),
+         'description'   => __( 'Explanations about the field info discount.', 'woocommerce' ),
+         'desc_tip'      => 'true',
+         'custom_attributes' => array(
+             'step' => 'any',
+             'min' => '1'
+         ),
+     ) );
+
+     echo '</div>';
+ }
+
+ // Save the data value from the custom fields for simple products
+ add_action( 'woocommerce_process_product_meta_simple', 'save_custom_fields_product_options_discount', 50, 1 );
+ function save_custom_fields_product_options_discount( $post_id ) {
+     // Save Number Field value
+     $number_field = $_POST['_discount_info'];
+
+     if( ! empty( $number_field ) ) {
+         update_post_meta( $post_id, '_discount_info', esc_attr( $number_field ) );
+     }
+ }
 
 
 
