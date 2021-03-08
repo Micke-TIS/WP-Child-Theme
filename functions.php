3f1712b81c6c28d664ea86fa00ba40a1911b4a8b
Micke <mikael.johanssonj@gmail.om>
2021-03-08 12:25:29 +0100

    d

--- functions.php
+++ functions.php
@@ -23,38 +23,41 @@ function sf_child_theme_dequeue_style() {
  * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
  */
 
- function woocommerce_product_custom_fields()
- {
-   $args = array(
-       'id' => 'woocommerce_custom_fields',
-       'label' => __('Add WooCommerce Custom Fields', 'cwoa'),
-   );
-   woocommerce_wp_text_input($args);
+ /**
+ * Displays the custom text field input field in the WooCommerce product data meta box
+ */
+ function cfwc_create_custom_field() {
+ $args = array(
+ 'id' => 'custom_text_field_title',
+ 'label' => __( 'Custom Text Field Title', 'cfwc' ),
+ 'class' => 'cfwc-custom-field',
+ 'desc_tip' => true,
+ 'description' => __( 'Enter the title of your custom text field.', 'ctwc' ),
+ );
+ woocommerce_wp_text_input( $args );
  }
+ add_action( 'woocommerce_product_options_general_product_data', 'cfwc_create_custom_field' );
+ /**
+ * Saves the custom field data to product meta data
+ */
+ function cfwc_save_custom_field( $post_id ) {
+ $product = wc_get_product( $post_id );
+ $title = isset( $_POST['custom_text_field_title'] ) ? $_POST['custom_text_field_title'] : '';
+ $product->update_meta_data( 'custom_text_field_title', sanitize_text_field( $title ) );
+ $product->save();
+ }
+ add_action( 'woocommerce_process_product_meta', 'cfwc_save_custom_field' );
 
- add_action('woocommerce_product_options_general_product_data', 'woocommerce_product_custom_fields');
-
-
- function save_woocommerce_product_custom_fields($post_id)
-{
-    $product = wc_get_product($post_id);
-    $custom_fields_woocommerce_title = isset($_POST['woocommerce_custom_fields']) ? $_POST['woocommerce_custom_fields'] : '';
-    $product->update_meta_data('woocommerce_custom_fields', sanitize_text_field($custom_fields_woocommerce_title));
-    $product->save();
+ /**
+* Displays custom field data after the add to cart button
+*/
+function cfwc_display_custom_field() {
+global $post;
+// Check for the custom field value
+$product = wc_get_product( $post->ID );
+$title = $product->get_meta( 'custom_text_field_title' );
+if( $title ) {
+echo get_post_meta($post->ID, 'custom_text_field_title', true);
 }
-add_action('woocommerce_process_product_meta', 'save_woocommerce_product_custom_fields');
-
-function woocommerce_custom_fields_display()
-{
-  global $post;
-  $product = wc_get_product($post->ID);
-    $custom_fields_woocommerce_title = $product->get_meta('woocommerce_custom_fields');
-  if ($custom_fields_woocommerce_title) {
-      printf(
-            '<div><label>%s</label><input type="text" id="woocommerce_product_custom_fields_title" name="woocommerce_product_custom_fields_title" value=""></div>',
-            esc_html($custom_fields_woocommerce_title)
-      );
-  }
 }
-
-add_action('woocommerce_before_add_to_cart_button', 'woocommerce_custom_fields_display');
+add_action( 'woocommerce_after_add_to_cart_button', 'cfwc_display_custom_field' );
